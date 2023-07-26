1919175c12cf9376afc18fee3345a90bf914512a
AngryAndrii <121435378+AngryAndrii@users.noreply.github.com>
2023-07-26 13:36:04 +0000

    Deploying to gh-pages from @ AngryAndrii/project-food1728@ac21f6e35d1a3e0577633ef98f06ab8e753ef6a8 🚀

---
+++ assets/favorites-bea60a7c.js
@@ -0,0 +1,4 @@
+import{r as s}from"./btn-scrollup-04fd150d.js";const i=document.querySelector(".favorites-list"),a=document.querySelector(".favorites-error"),t=document.querySelector(".favorites-categories"),n=document.querySelector(".favorites-hero"),e=JSON.parse(localStorage.getItem("favorites"));function c(){if(i.insertAdjacentHTML("beforeend",s(e)),e.length===0){a.classList.remove("visually-hidden"),t.classList.add("visually-hidden"),window.innerWidth<767&&n.classList.add("visually-hidden");return}}c();function d(){const r=e.map(o=>`<button class="favorites-btn btn-categori is-active"
+                  type="button">${o.category}
+                </button>
+                `).join("");t.insertAdjacentHTML("beforeend",r)}d();
